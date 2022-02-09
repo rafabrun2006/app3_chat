e3ef550e4a83dcc5b1f737cf95f8fe8c02839e76
Rafael Bruno <rafabrun2006@gmail.com>
2022-02-09 19:34:12 -0300

    Melhoria de codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { initializeApp } from "firebase/app";
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    initializeApp({
+      apiKey: "AIzaSyB-HEUlWiEtokt8uI0oS2aX-gxQXQpi1AQ",
+      authDomain: "chat-9fcd5.firebaseapp.com",
+      projectId: "chat-9fcd5",
+      storageBucket: "chat-9fcd5.appspot.com",
+      messagingSenderId: "433694011323",
+      appId: "1:433694011323:web:8430616a7c6622c4851e9e"
+    });
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,9 +2,7 @@
 
 import { Component, NgZone } from '@angular/core';
 import { LoadingController, ModalController } from '@ionic/angular';
-import { initializeApp } from "firebase/app";
 import { getDatabase, ref, push, set, onValue, onChildChanged } from "firebase/database";
-import { getFirestore, getDocs, collection, addDoc, onSnapshot } from "firebase/firestore"
 
 @Component({
   selector: 'app-home',
@@ -12,24 +10,13 @@ import { getFirestore, getDocs, collection, addDoc, onSnapshot } from "firebase/
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  
+
   public messages: Array<any>;
   public message: string;
   public messagesRef;
   public username;
 
   constructor(public _zone: NgZone, public loadingController: LoadingController) {
-
-    const firebaseConfig = {
-      apiKey: "AIzaSyB-HEUlWiEtokt8uI0oS2aX-gxQXQpi1AQ",
-      authDomain: "chat-9fcd5.firebaseapp.com",
-      projectId: "chat-9fcd5",
-      storageBucket: "chat-9fcd5.appspot.com",
-      messagingSenderId: "433694011323",
-      appId: "1:433694011323:web:8430616a7c6622c4851e9e"
-    };
-
-    initializeApp(firebaseConfig);
 
     const db = getDatabase();
     this.messagesRef = ref(db, 'messages');
@@ -40,10 +27,10 @@ export class HomePage {
       localStorage.setItem('username', String(this.username));
     }
 
-    this.load();
+    this.loadMessages();
   }
 
-  public async load() {
+  public async loadMessages() {
     const loading = await this.loadingController.create({message: 'Carregando mensagens...'});
     loading.present();
 
